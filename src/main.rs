e40d2c7571ed3e76a7622a31aec06f0c66b925d2
Aart Stuurman <aartstuurman@hotmail.com>
2018-07-20 01:17:46 +0200

    ast

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ enum Opcode {
     Halt
 }
 
-struct ExtProcRef {
+struct TokenExtProcRef {
     module: String,
     procedure: String
 }
@@ -36,7 +36,7 @@ enum Token {
     Opcode(Opcode),
     Label(String),
     LabelRef(String),
-    ExtProcRef(ExtProcRef),
+    ExtProcRef(TokenExtProcRef),
     End,
     EOF
 }
@@ -134,7 +134,7 @@ fn text_to_extprocref(text: &str) -> Option<Token> {
                     !procedure.chars().next().unwrap().is_lowercase() {
                     return None;
                 }
-                return Some(Token::ExtProcRef(ExtProcRef{
+                return Some(Token::ExtProcRef(TokenExtProcRef{
                     module: module.to_string(),
                     procedure: procedure.to_string()
                 }));
@@ -230,7 +230,19 @@ fn lexer(source: &str) -> Vec<Token> {
 }
 
 enum Operation {
-
+    Jmp(Jmp),
+    Jmps,
+    JmpTrue(JmpTrue),
+    CmpU8,
+    Spi(Spi),
+    Spd(Spd),
+    PushU8(PushU8),
+    PushU64(PushU64),
+    PopU8,
+    SetU8(SetU8),
+    CplU8(CplU8),
+    CpgU8(CpgU8),
+    Halt
 }
 
 enum Rule {
@@ -244,13 +256,291 @@ struct Procedure {
 }
 
 struct Program {
-    procedures: HashMap<String, Procedure>
+    procedures: Vec<(String, Procedure)>
 }
 
-fn parse_rule(source: &[Token]) -> Option<(Rule, &[Token])> {
-    let mut source_leftover = source;
+enum Address {
+    Label(String),
+    IntLiteral(u64),
+    ExtProcRef(ExtProcRef)
+}
 
-    return Some((Rule::Label("testlabel".to_string()), &source_leftover[1..])); // TODO
+struct ExtProcRef {
+    module: String,
+    procedure: String
+}
+
+struct CpgU8 {
+    address: Address
+}
+
+struct CplU8 {
+    address: Address
+}
+
+struct Jmp {
+    address: Address
+}
+
+struct JmpTrue {
+    address: Address
+}
+
+struct PushU64 {
+    address: Address
+}
+
+struct PushU8 {
+    value: u64
+}
+
+struct SetU8 {
+    address: Address
+}
+
+struct Spd {
+    value: u64
+}
+
+struct Spi {
+    value: u64
+}
+
+fn parse_cmp_u8(source: &[Token]) -> Option<(Rule, &[Token])> {
+    return Some((Rule::Operation(Operation::CmpU8), source));
+}
+
+fn parse_cpg_u8(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::CpgU8(CpgU8{address: Address::IntLiteral(*value)});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::LabelRef(value) => {
+            let op = Operation::CpgU8(CpgU8{address: Address::Label(value.to_string())});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::ExtProcRef(value) => {
+            let op = Operation::CpgU8(CpgU8{address: Address::ExtProcRef(ExtProcRef{module: value.module.to_string(), procedure: value.procedure.to_string() })});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_cpg_u8");
+            return None;
+        }
+    }
+}
+
+fn parse_cpl_u8(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::CplU8(CplU8{address: Address::IntLiteral(*value)});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::LabelRef(value) => {
+            let op = Operation::CplU8(CplU8{address: Address::Label(value.to_string())});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::ExtProcRef(value) => {
+            let op = Operation::CplU8(CplU8{address: Address::ExtProcRef(ExtProcRef{module: value.module.to_string(), procedure: value.procedure.to_string() })});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_cpl_u8");
+            return None;
+        }
+    }
+}
+
+fn parse_halt(source: &[Token]) -> Option<(Rule, &[Token])> {
+    return Some((Rule::Operation(Operation::Halt), source));
+}
+
+fn parse_jmp(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::Jmp(Jmp{address: Address::IntLiteral(*value)});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::LabelRef(value) => {
+            let op = Operation::Jmp(Jmp{address: Address::Label(value.to_string())});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::ExtProcRef(value) => {
+            let op = Operation::Jmp(Jmp{address: Address::ExtProcRef(ExtProcRef{module: value.module.to_string(), procedure: value.procedure.to_string() })});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_jmp");
+            return None;
+        }
+    }
+}
+
+fn parse_jmps(source: &[Token]) -> Option<(Rule, &[Token])> {
+    return Some((Rule::Operation(Operation::Jmps), source));
+}
+
+fn parse_jmp_true(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::JmpTrue(JmpTrue{address: Address::IntLiteral(*value)});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::LabelRef(value) => {
+            let op = Operation::JmpTrue(JmpTrue{address: Address::Label(value.to_string())});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::ExtProcRef(value) => {
+            let op = Operation::JmpTrue(JmpTrue{address: Address::ExtProcRef(ExtProcRef{module: value.module.to_string(), procedure: value.procedure.to_string() })});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_jmp_true");
+            return None;
+        }
+    }
+}
+
+fn parse_pop_u8(source: &[Token]) -> Option<(Rule, &[Token])> {
+    return Some((Rule::Operation(Operation::PopU8), source));
+}
+
+fn parse_push_u64(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::PushU64(PushU64{address: Address::IntLiteral(*value)});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::LabelRef(value) => {
+            let op = Operation::PushU64(PushU64{address: Address::Label(value.to_string())});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::ExtProcRef(value) => {
+            let op = Operation::PushU64(PushU64{address: Address::ExtProcRef(ExtProcRef{module: value.module.to_string(), procedure: value.procedure.to_string() })});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_push_u64");
+            return None;
+        }
+    }
+}
+
+fn parse_push_u8(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::PushU8(PushU8{value: *value});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_push_u8");
+            return None;
+        }
+    }
+}
+
+fn parse_set_u8(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::SetU8(SetU8{address: Address::IntLiteral(*value)});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::LabelRef(value) => {
+            let op = Operation::SetU8(SetU8{address: Address::Label(value.to_string())});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        Token::ExtProcRef(value) => {
+            let op = Operation::SetU8(SetU8{address: Address::ExtProcRef(ExtProcRef{module: value.module.to_string(), procedure: value.procedure.to_string() })});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_set_u8");
+            return None;
+        }
+    }
+}
+
+fn parse_spd(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::Spd(Spd{value: *value});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_spd");
+            return None;
+        }
+    }
+}
+
+fn parse_spi(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::IntLiteral(value) => {
+            let op = Operation::Spi(Spi{value: *value});
+            return Some((Rule::Operation(op), &source[1..]));
+        },
+        _ => {
+            println!("parse_spi");
+            return None;
+        }
+    }
+}
+
+
+fn parse_rule(source: &[Token]) -> Option<(Rule, &[Token])> {
+    match &source[0] {
+        Token::Opcode(opcode) => {
+            match opcode {
+                Opcode::CmpU8 => {
+                    return parse_cmp_u8(&source[1..]);
+                },
+                Opcode::CpgU8 => {
+                    return parse_cpg_u8(&source[1..]);
+                },
+                Opcode::CplU8 => {
+                    return parse_cpl_u8(&source[1..]);
+                },
+                Opcode::Halt => {
+                    return parse_halt(&source[1..]);
+                },
+                Opcode::Jmp => {
+                    return parse_jmp(&source[1..]);
+                },
+                Opcode::Jmps => {
+                    return parse_jmps(&source[1..]);
+                },
+                Opcode::JmpTrue => {
+                    return parse_jmp_true(&source[1..]);
+                },
+                Opcode::PopU8 => {
+                    return parse_pop_u8(&source[1..]);
+                },
+                Opcode::PushU64 => {
+                    return parse_push_u64(&source[1..]);
+                },
+                Opcode::PushU8 => {
+                    return parse_push_u8(&source[1..]);
+                },
+                Opcode::SetU8 => {
+                    return parse_set_u8(&source[1..]);
+                },
+                Opcode::Spd => {
+                    return parse_spd(&source[1..]);
+                },
+                Opcode::Spi => {
+                    return parse_spi(&source[1..]);
+                }
+            }
+        },
+        Token::Label(label) => {
+            return Some((Rule::Label(label.to_string()), &source[1..]));
+        },
+        _ => {
+            println!("nothing");
+            return None;
+        }
+    }
 }
 
 fn parse_proc(source: &[Token]) -> Option<(String, Procedure, &[Token])> {
@@ -278,6 +568,9 @@ fn parse_proc(source: &[Token]) -> Option<(String, Procedure, &[Token])> {
     let mut labels: HashMap<String, usize> = HashMap::new();
     let mut operations: Vec<Operation> = Vec::new();    
     while std::mem::discriminant(&source_leftover[0]) != std::mem::discriminant(&Token::End) {
+        while std::mem::discriminant(&source_leftover[0]) == std::mem::discriminant(&Token::NewLine) {
+            source_leftover = &source_leftover[1..];
+        }
         match parse_rule(source_leftover) {
             Some((rule, leftover)) => {
                 match rule {
@@ -291,6 +584,9 @@ fn parse_proc(source: &[Token]) -> Option<(String, Procedure, &[Token])> {
                 source_leftover = leftover;
             }
             None => return None
+        }
+        while std::mem::discriminant(&source_leftover[0]) == std::mem::discriminant(&Token::NewLine) {
+            source_leftover = &source_leftover[1..];
         }
     }
     
@@ -306,14 +602,13 @@ fn parse_proc(source: &[Token]) -> Option<(String, Procedure, &[Token])> {
 }
 
 fn parser(source: &Vec<Token>) -> Program {
-    let mut procedures: HashMap<String, Procedure> = HashMap::new();
+    let mut procedures: Vec<(String, Procedure)> = Vec::new();
 
    let mut source_leftover = source.as_slice();
     while std::mem::discriminant(&source_leftover[0]) != std::mem::discriminant(&Token::EOF) {
         match parse_proc(source_leftover) {
             Some((name, procedure, leftover)) => {
-                println!("proc: {}", name);
-                procedures.insert(name, procedure);
+                procedures.push((name, procedure));
                 source_leftover = leftover;
             },
             None => panic!("Invalid procedure")
